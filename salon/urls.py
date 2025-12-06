f8921679d88fb525a2ef732e05cb5054dae5b245
Andre8711 <1andrey171@mail.ru>
2025-12-06 19:10:48 +0300

    Задание 2: Детали салона с обработкой исключений

--- salon/urls.py
+++ salon/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.active_salons, name='home'),  # Главная страница
     path('active_salons/', views.active_salons, name='active_salons'),
-    # другие маршруты
+    path('salon/<int:salon_id>/', views.salon_detail, name='salon_detail'),
 ]
--- salon/views.py
+++ salon/views.py
@@ -1,7 +1,13 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Salon
+
 
 def active_salons(request):
     salons = Salon.objects.filter(is_active=True)
     return render(request, 'salon/active_salons.html', {'salons': salons})
+    
+
+def salon_detail(request, salon_id):
+    salon = get_object_or_404(Salon, id=salon_id)
+    return render(request, 'salon/salon_detail.html', {'salon': salon})
